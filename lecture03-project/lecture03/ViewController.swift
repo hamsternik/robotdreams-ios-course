c48fb49dcb491ecd3bfaac05567cc4a22abb3cc3
Nikita Khomitsevich <hamsternik9@gmail.com>
2021-02-22 16:13:51 +0200

    add work project from lecture 3

---
+++ lecture03-project/lecture03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lecture03
+//
+//  Created by hamsternik on 20.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
