8f39d511f3b9ae9a084de80885aa9b9168cfcc8c
Nikita Khomitsevich <hamsternik9@gmail.com>
2021-03-04 01:02:05 +0200

    #22: add lecture project skeleton

---
+++ lecture-6/lecture06-project/project06-settings-final/project06-settings-final/SettingTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  project06-settings-final
+//
+//  Created by hamsternik on 03.03.2021.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
