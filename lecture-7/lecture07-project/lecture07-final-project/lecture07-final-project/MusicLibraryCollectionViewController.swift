ce5e9ba4a95de70af0d85b79979ec01e5ad87ef5
Nikita Khomitsevich <hamsternik9@gmail.com>
2021-03-09 16:53:35 +0200

    #28: WIP xcode project + mockup

---
+++ lecture-7/lecture07-project/lecture07-final-project/lecture07-final-project/MusicLibraryCollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MusicLibraryCollectionViewController.swift
+//  lecture07-final-project
+//
+//  Created by hamsternik on 09.03.2021.
+//
+
+import UIKit
+
+class MusicLibraryCollectionViewController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
