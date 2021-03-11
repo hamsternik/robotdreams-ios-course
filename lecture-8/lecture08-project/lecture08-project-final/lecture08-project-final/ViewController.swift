e26a7b7d64ee4a0ffd094f5117ba728d8d521ec7
Nikita Khomitsevich <hamsternik9@gmail.com>
2021-03-11 21:56:13 +0200

    #33: WIP project

---
+++ lecture-8/lecture08-project/lecture08-project-final/lecture08-project-final/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lecture08-project-final
+//
+//  Created by hamsternik on 11.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
