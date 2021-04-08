c32ef4dfab3be3b7456b5f59235c26a054021ce8
Nikita Khomitsevich <hamsternik9@gmail.com>
2021-04-08 05:08:47 +0300

    #60, lecture16, add project skeleton

---
+++ lecture-16/lecture16-project/lecture16-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lecture16-project
+//
+//  Created by hamsternik on 08.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    private let userDefaults = UserDefaults.standard
+}
+
---
+++ lecture-16/lecture16-project/lecture16-projectTests/lecture16_projectTests.swift
@@ -0,0 +1,33 @@
+//
+//  lecture16_projectTests.swift
+//  lecture16-projectTests
+//
+//  Created by hamsternik on 08.04.2021.
+//
+
+import XCTest
+@testable import lecture16_project
+
+class lecture16_projectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
