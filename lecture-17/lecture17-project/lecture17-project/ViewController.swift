1b4eeef645b61197a8ff353bdbb90eaf463b780d
Nikita Khomitsevich <hamsternik9@gmail.com>
2021-04-12 04:16:40 +0300

    #63, lecture17, project skeleton

---
+++ lecture-17/lecture17-project/lecture17-project/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  lecture17-project
+//
+//  Created by hamsternik on 12.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        /// TBD
+    }
+}
+
